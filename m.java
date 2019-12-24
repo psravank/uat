8560230d2d9436a0951e030d6e02b3ca9b4d223d
psravan <psravan@OJAS-DD56.OJASIDM.COM>
2019-12-24 18:01:32 +0530

    commit

---
+++ m.java
@@ -0,0 +1,14 @@
+class Simple{
+	//public static void main(String[] args)  
+	//public static void main(String []args)  
+	//public static void main(String args[])  
+	//public static void main(String... args)  
+	//static public void main(String[] args)  
+	//public static final void main(String[] args)  
+	final public static void main(String[] args)  
+	//final strictfp public static void main(String[] args){  
+        	System.out.println("Hello Java");  
+			System.out.println("Hello Java");
+			System.out.println("Java the");
+        }  
+    }  
